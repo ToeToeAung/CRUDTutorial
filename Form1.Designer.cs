2a7719a5208af9ad656724990b42c10e05550a8a
toeto <toeto@LAPTOP-8DFC5J3T>
2024-01-13 12:35:51 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CRUDTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnSave = new Button();
+            label1 = new Label();
+            txtName = new TextBox();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            txtPhone = new TextBox();
+            label3 = new Label();
+            txtAddress = new TextBox();
+            label4 = new Label();
+            lblID = new Label();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 238);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(561, 257);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(168, 198);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 1;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(168, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(229, 12);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(229, 57);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(100, 23);
+            txtEmail.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(168, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Email";
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(229, 106);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(100, 23);
+            txtPhone.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(168, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Phone";
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(229, 159);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(100, 23);
+            txtAddress.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(168, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Address";
+            // 
+            // lblID
+            // 
+            lblID.AutoSize = true;
+            lblID.Location = new Point(61, 65);
+            lblID.Name = "lblID";
+            lblID.Size = new Size(38, 15);
+            lblID.TabIndex = 10;
+            lblID.Text = "label5";
+            lblID.Visible = false;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(265, 198);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 11;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(355, 198);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 12;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(585, 507);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(lblID);
+            Controls.Add(txtAddress);
+            Controls.Add(label4);
+            Controls.Add(txtPhone);
+            Controls.Add(label3);
+            Controls.Add(txtEmail);
+            Controls.Add(label2);
+            Controls.Add(txtName);
+            Controls.Add(label1);
+            Controls.Add(btnSave);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnSave;
+        private Label label1;
+        private TextBox txtName;
+        private TextBox txtEmail;
+        private Label label2;
+        private TextBox txtPhone;
+        private Label label3;
+        private TextBox txtAddress;
+        private Label label4;
+        private Label lblID;
+        private Button btnUpdate;
+        private Button btnDelete;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUD.BLL;
+using CRUD.Entities;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CRUDTutorial
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["crudConnectionStr"].ConnectionString;
+        CRUDBLL cRUDBLL = new CRUDBLL();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = cRUDBLL.getStudent(connectionString);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student = bindStudent();
+            cRUDBLL.insertStudent(connectionString, student);
+            dataGridView1.DataSource = cRUDBLL.getStudent(connectionString);
+        }
+
+        private Student bindStudent()
+        {
+            Student student = new Student();
+            student.ID = Convert.ToInt32(lblID.Text);
+            student.Name = txtName.Text;
+            student.Email = txtEmail.Text;
+            student.Phone = txtPhone.Text;
+            student.Address = txtAddress.Text;
+            return student;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+
+                int selectedRowIndex = e.RowIndex;
+                int id = (int)dataGridView1.Rows[selectedRowIndex].Cells["ID"].Value;
+                string name = dataGridView1.Rows[selectedRowIndex].Cells["Name"].Value.ToString();
+                string email = dataGridView1.Rows[selectedRowIndex].Cells["Email"].Value.ToString();
+                string phone = dataGridView1.Rows[selectedRowIndex].Cells["Phone"].Value.ToString();
+                string add = dataGridView1.Rows[selectedRowIndex].Cells["Address"].Value.ToString();
+
+                lblID.Text = id.ToString();
+                txtName.Text = name;
+                txtEmail.Text = email;
+                txtPhone.Text = phone;
+                txtAddress.Text = add;
+
+
+                //MessageBox.Show($"Cell Double-Clicked!\nRow: {e.RowIndex + 1}\nColumn: {e.ColumnIndex + 1}\nValue: {cellValue}");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student = bindStudent();
+            cRUDBLL.updateStudent(connectionString, student);
+            dataGridView1.DataSource = cRUDBLL.getStudent(connectionString);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int ID= Convert.ToInt32(lblID.Text);
+            if(ID>0) {
+                cRUDBLL.deleteStudent(connectionString, ID);
+                dataGridView1.DataSource = cRUDBLL.getStudent(connectionString);
+            }           
+        }
+    }
+}
